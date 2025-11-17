9917288e7eac4c9ccd91f6bda701e1ba62528caa
joeyscags <joeyscags5000@gmail.com>
2025-11-17 17:37:33 +0100

    added main to implement the Zombiehorde function and it seems to work properly will have to check for leaks another time

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,12 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcupp <jcupp@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*   By: joeyscags <jcupp@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 16:29:03 by jcupp             #+#    #+#             */
-/*   Updated: 2025/11/17 16:29:04 by jcupp            ###   ########.fr       */
+/*   Updated: 2025/11/17 17:36:37 by joeyscags        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+int main() {
+    int N = 5;
+    std::string name = "HordeZombie";
+    Zombie* horde = zombieHorde(N, name);
+    for (int i = 0; i < N; i++) {
+        horde[i].announce();
+    }
+    delete[] horde;
+    return 0;
+}
 
 
 
