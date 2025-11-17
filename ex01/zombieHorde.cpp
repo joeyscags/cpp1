75d80f7c703d838c5483a57159592443c31fe37d
joeyscags <joeyscags5000@gmail.com>
2025-11-17 17:40:42 +0100

    changed Nullptr to Null since it didnt compile. Should figure out why it doesnt work when i researched it said nullptr was good practice

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   zombieHorde.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jcupp <jcupp@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*   By: joeyscags <jcupp@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 16:29:40 by jcupp             #+#    #+#             */
-/*   Updated: 2025/11/17 16:29:43 by jcupp            ###   ########.fr       */
+/*   Updated: 2025/11/17 17:38:29 by joeyscags        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 Zombie* zombieHorde(int N, std::string name) {
 	if (N <= 0)
-		return nullptr;
+		return NULL;
 	Zombie* horde = new Zombie[N];
 	for (int i = 0; i < N; i++) {
 		horde[i].setName(name);
