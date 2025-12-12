020e6da1148cebadf43333861dd53a71718f5947
joeyscags <joeyscags5000@gmail.com>
2025-12-12 17:03:21 +0100

    implemented finsihed main i think just need to make the replaceAll function

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,28 +1,54 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: joeyscags <jcupp@student.42heilbronn.de    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/11 19:29:04 by jcupp             #+#    #+#             */
-/*   Updated: 2025/12/11 19:44:18 by joeyscags        ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+	/* ************************************************************************** */
+	/*                                                                            */
+	/*                                                        :::      ::::::::   */
+	/*   main.cpp                                           :+:      :+:    :+:   */
+	/*                                                    +:+ +:+         +:+     */
+	/*   By: joeyscags <jcupp@student.42heilbronn.de    +#+  +:+       +#+        */
+	/*                                                +#+#+#+#+#+   +#+           */
+	/*   Created: 2025/12/11 19:29:04 by jcupp             #+#    #+#             */
+	/*   Updated: 2025/12/11 20:01:03 by joeyscags        ###   ########.fr       */
+	/*                                                                            */
+	/* ************************************************************************** */
 
-#include <fstream>
-#include <iostream>
-#include <string>
+	#include <cstdlib>
+	#include <fstream>
+	#include <iostream>
+	#include <sstream>
+	#include <string>
 
-int	main(int argc, char **argv)
-{
-	if (argc != 4)
+	int	main(int argc, char **argv)
 	{
-		std::cerr << "Usage: " << argv[0] << " <filename> <s1> <s2>" << std::endl;
-		return (1);
+		if (argc != 4)
+		{
+			std::cerr << "Usage: " << argv[0] << " <filename> <s1> <s2>" << std::endl;
+			return (EXIT_FAILURE);
+		}
+		std::string filename = argv[1];
+		std::string s1 = argv[2];
+		std::string s2 = argv[3];
+		std::ifstream inputFile(filename);
+		if (!inputFile)
+		{
+			std::cerr << "Error: Could not open input file '" << filename << "'." << std::endl;
+			return (EXIT_FAILURE);
+		}
+		std::ostringstream buffer;
+		buffer << inputFile.rdbuf();
+		std::string content = buffer.str();
+		inputFile.close();
+		std::string replaced;
+		if (!replaceAll(content, s1, s2, replaced))
+		{
+			std::cerr << "Error: Failed to process file." << std::endl;
+			return (EXIT_FAILURE);
+		}
+		std::ofstream outputFile(filename + ".replace");
+		if (!outputFile)
+		{
+			std::cerr << "Error: Could not open output file." << std::endl;
+			return (EXIT_FAILURE);
+		}
+		outputFile << replaced;
+		outputFile.close();
+		return (EXIT_SUCCESS);
 	}
-	std::string filename = argv[1];
-	std::string s1 = argv[2];
-	std::string s2 = argv[3];
-	return (0);
-}
