1c14f351cf22a00071aa85678a6882f8e70db725
joeyscags <joeyscags5000@gmail.com>
2025-12-12 19:41:37 +0100

    wrote easy main to test if the code works and it seems to work!

---
+++ ex05/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joeyscags <jcupp@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/12 18:40:00 by joeyscags         #+#    #+#             */
+/*   Updated: 2025/12/12 19:41:13 by joeyscags        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int	main(void)
+{
+	Harl	harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+	harl.complain("INVALID");
+	return (0);
+}
